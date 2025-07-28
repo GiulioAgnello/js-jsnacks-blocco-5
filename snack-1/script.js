4384c608bc1354c05ee728711341ec495718e6e8
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-07-28 10:43:12 +0200

    clear2

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,6 +1,1 @@
-const names = ["Edoardo", "Simone", "Francesco"];
 
-// Stampa in console tutti i nomi
-// Risultato: 'Edoardo', 'Simone', 'Francesco'
-
-names.forEach((element) => console.log(element));
